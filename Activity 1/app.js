ec2aa2a2d2e7a2f78afe2691e0ed7c298f7a621b
Fernando Castillo <fcastillocosme@gmail.com>
2020-09-17 16:11:41 -0500

    Commit despues de revision de Jesus; const, pascal case y prettier

--- Activity 1/app.js
+++ Activity 1/app.js
@@ -1,25 +1,89 @@
-let list = [
-  { id: 1, name: "Fernando", lastname: "Castillo", email: "fcastillocosme@gmail.com", position: "Associate Software Engineer" },
-  { id: 2, name: "Arturo", lastname: "Cantu", email: "a.cantu@gmail.com", position: "Associate Software Engineer" },
-  { id: 3, name: "Simon", lastname: "Arenas", email: "simon.arenas@gmail.com", position: "Associate Software Engineer" },
-  { id: 4, name: "Cesar", lastname: "Vega", email: "cesar.vega@gmail.com", position: "Associate Software Engineer" },
-  { id: 5, name: "Alejandro", lastname: "Gonzales", email: "alejando.g@gmail.com", position: "Associate Software Engineer" },
-  { id: 6, name: "Rafael", lastname: "Martinez", email: "rafa.mar@theksquaregroup.com", position: "Software Engineer" },
-  { id: 7, name: "Beatriz", lastname: "Osorio", email: "beatriz.osorio@theksquaregroup.com", position: "Software Engineer" },
-  { id: 8, name: "Magaly", lastname: "Cruz", email: "magalymagaly@theksquaregroup.com", position: "Software Engineer" },
-  { id: 9, name: "Jesus", lastname: "Berzunza", email: "jesusjesus@theksquaregroup.com", position: "Software Engineer" },
-  { id: 10, name: "Varma", lastname: "Perez", email: "vharmavharma@theksquaregroup.com", position: "Software Engineer" }
+const list = [
+  {
+    id: 1,
+    name: "Fernando",
+    lastName: "Castillo",
+    email: "fcastillocosme@gmail.com",
+    position: "Associate Software Engineer",
+  },
+  {
+    id: 2,
+    name: "Arturo",
+    lastName: "Cantu",
+    email: "a.cantu@gmail.com",
+    position: "Associate Software Engineer",
+  },
+  {
+    id: 3,
+    name: "Simon",
+    lastName: "Arenas",
+    email: "simon.arenas@gmail.com",
+    position: "Associate Software Engineer",
+  },
+  {
+    id: 4,
+    name: "Cesar",
+    lastName: "Vega",
+    email: "cesar.vega@gmail.com",
+    position: "Associate Software Engineer",
+  },
+  {
+    id: 5,
+    name: "Alejandro",
+    lastName: "Gonzales",
+    email: "alejando.g@gmail.com",
+    position: "Associate Software Engineer",
+  },
+  {
+    id: 6,
+    name: "Rafael",
+    lastName: "Martinez",
+    email: "rafa.mar@theksquaregroup.com",
+    position: "Software Engineer",
+  },
+  {
+    id: 7,
+    name: "Beatriz",
+    lastName: "Osorio",
+    email: "beatriz.osorio@theksquaregroup.com",
+    position: "Software Engineer",
+  },
+  {
+    id: 8,
+    name: "Magaly",
+    lastName: "Cruz",
+    email: "magalymagaly@theksquaregroup.com",
+    position: "Software Engineer",
+  },
+  {
+    id: 9,
+    name: "Jesus",
+    lastName: "Berzunza",
+    email: "jesusjesus@theksquaregroup.com",
+    position: "Software Engineer",
+  },
+  {
+    id: 10,
+    name: "Varma",
+    lastName: "Perez",
+    email: "vharmavharma@theksquaregroup.com",
+    position: "Software Engineer",
+  },
 ];
 
-list.forEach(user => user.position == "Software Engineer" ? user["is training"] = false : user["is training"] = true);
+list.forEach((user) =>
+  user.position == "Software Engineer"
+    ? (user.isTraining = false)
+    : (user.isTraining = true)
+);
 
-function showByAttribute(attribute, filter) {      
-  return list.filter(user => user[attribute] == filter);
+function showByProperty(property, filter) {
+  return list.filter((user) => user[property] == filter);
 }
 
-console.log('Show by position: Software Engineer');
-console.log(showByAttribute("position", "Software Engineer"));
-console.log('\nShow by id: 1');
-console.log(showByAttribute("id", "1"));
-console.log('\nShow by name: Vharma');
-console.log(showByAttribute("name", "Varma"));
+console.log("Show by position: Software Engineer");
+console.log(showByProperty("position", "Software Engineer"));
+console.log("\nShow by id: 1");
+console.log(showByProperty("id", "1"));
+console.log("\nShow by name: Varma");
+console.log(showByProperty("name", "Varma"));

9558ca617c927709048f10bb7ec6f85e6636d3ef
cv <g.vega.cl@gmail.com>
2020-09-17 12:12:06 -0500

    created the class, also testing branch

--- Cesar_vega/2020_09_27-JavascriptActivity.js
+++ Cesar_vega/2020_09_27-JavascriptActivity.js
@@ -1,4 +1,10 @@
 class user{
-    
+    constructor(id,name,last_name,email,position) {
+        this.id = id;
+        this.name = name;
+        this.last_name;
+        this.email = email;
+        this.position = position;
+    }
 }
 user_list = []
